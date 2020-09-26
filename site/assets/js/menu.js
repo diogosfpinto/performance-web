c1564e14b39d45d4870f535467f6fc476aaf94b5
diogosfpinto <diogo.sfpinto@gmail.com>
2020-09-26 16:38:28 -0400

    Revert "Removed code jquery of menu.js"
    This reverts commit 80007c244d503abcc3fdce91069c6885f618cbf0.

--- site/assets/js/menu.js
+++ site/assets/js/menu.js
@@ -1,14 +1,12 @@
-window.addEventListener('load', function() {
-    var titulo = document.querySelector('.header-menu-titulo');
-    var menu = document.querySelector('.header-menu');
+$(window).on('load', function() {
+	var menu = $('.header-menu');
 
-    if (titulo) {
-        titulo.onclick = function() {
-            if (menu.hasAttribute('data-ativo')) {
-                menu.removeAttribute('data-ativo');
-            } else {
-                menu.setAttribute('data-ativo', '');
-            }
-        };
-    }
+	$('.header-menu-titulo').on('click', function() {
+		if (menu.is('[data-ativo]')) {
+			menu.removeAttr('data-ativo');
+		} else {
+			menu.attr('data-ativo', '');
+		}
+	});
+
 });
