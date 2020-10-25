a23bbf442fb9132f45fc83636922d8678af98967
diogosfpinto <diogo.sfpinto@gmail.com>
2020-10-25 18:23:13 -0400

    loading video after 3 seconds

--- site/assets/js/video.js
+++ site/assets/js/video.js
@@ -1,10 +1,9 @@
 // carrega videos assincronamente
 setTimeout(function(){
 
-	var containers = document.querySelectorAll('.elasticMedia-container[data-src]');
-	for (var i = 0; i < containers.length; i++) {
-		var src = containers[i].getAttribute('data-src');
-		containers[i].innerHTML = '<iframe class="elasticMedia" src="' + src + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-	}
+	console.log('video.js')
+	var iframe = '<iframe class="elasticMedia" src="https://player.vimeo.com/video/99185150" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 
-}, 500);
+	document.querySelector('.elasticMedia-container').innerHTML = iframe;
+
+}, 3000);
