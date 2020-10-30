a5446053e159a3b5eb38f5f06e32e395d3579720
diogosfpinto <diogo.sfpinto@gmail.com>
2020-10-29 21:07:21 -0400

    removed querySelectAll of loop

--- site/assets/js/lazyload.js
+++ site/assets/js/lazyload.js
@@ -1,10 +1,21 @@
-window.onscroll = function(){
-    
-    var imgs = document.querySelectorAll('img[data-src]');
+var jarodei = false;
 
+var imgs = document.querySelectorAll('img[data-src]');
+
+var heightPage = window.innerHeight;
+
+window.onscroll = function(){
+
+    // throttle
+    if (jarodei) return;
+    jarodei = true;
+    setTimeout(function(){
+        jarodei = false;
+    }, 100);
+    
     for (var i = 0 ; i < imgs.length ; i++ ){
 
-        if (imgs[i].getBoundingClientRect().top < window.innerHeight){
+        if (imgs[i].getBoundingClientRect().top < heightPage + 200){
             imgs[i].src = imgs[i].getAttribute('data-src');
         }        
     }
